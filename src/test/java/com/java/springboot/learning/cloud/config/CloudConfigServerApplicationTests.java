4783c894493e29a9d6f9634ebf0cdc230b98789b
BhabaniN <mrnayak.bhabanishankar@gmail.com>
2022-04-21 10:28:18 +0530

    setting up spring cloud config server

---
+++ src/test/java/com/java/springboot/learning/cloud/config/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springboot.learning.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
